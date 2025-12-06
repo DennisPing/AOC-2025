ae5cba28ab1b821b26cf1d8e7e9daca5681c5817
Dennis Ping <idennisping@gmail.com>
2025-12-06 12:03:14 -0500

    Completed day 3 part 1

---
+++ Day3/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	seqs := parseInput("input.txt")
+	part1(seqs)
+}
+
+func part1(seqs [][]int) {
+	sum := 0
+	for _, seq := range seqs {
+		val := findTwoLargest(seq)
+		sum += val
+	}
+	fmt.Println(sum)
+}
+
+func findTwoLargest(seq []int) int {
+	m := 0
+
+	// Find the largest tenth digit
+	for i := 0; i < len(seq)-1; i++ {
+		if seq[i] > seq[m] {
+			m = i
+		}
+	}
+
+	// Find the largest single digit after index m
+	n := m + 1
+	for i := m + 1; i < len(seq); i++ {
+		if seq[i] > seq[n] {
+			n = i
+		}
+	}
+
+	concatStr := fmt.Sprintf("%d%d", seq[m], seq[n])
+	concatInt, _ := strconv.Atoi(concatStr)
+	return concatInt
+}
+
+func parseInput(filename string) [][]int {
+	data, err := os.ReadFile(filename)
+	if err != nil || len(data) == 0 {
+		log.Fatalln("unable to read file:", filename)
+	}
+
+	lines := strings.SplitSeq(string(data), "\n")
+
+	var seqs [][]int
+	for line := range lines {
+		line = strings.TrimSpace(line)
+		var seq []int
+		for _, v := range line {
+			joltage, _ := strconv.Atoi(string(v))
+			seq = append(seq, joltage)
+		}
+
+		seqs = append(seqs, seq)
+	}
+
+	return seqs
+}
