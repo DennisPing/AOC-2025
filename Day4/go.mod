37d79d86d57ede457740fe74591cbd35560fd016
Dennis Ping <idennisping@gmail.com>
2025-12-06 19:57:53 -0500

    Completed day 4

---
+++ Day4/go.mod
@@ -0,0 +1,3 @@
+module github.com/DennisPing/AOC-2025/day4
+
+go 1.25.4
---
+++ Day4/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	grid := parseInput("input.txt")
+
+	part1(grid)
+	part2(grid)
+}
+
+func part1(grid [][]rune) {
+	total := 0
+	for r := 1; r < len(grid)-1; r++ {
+		for c := 1; c < len(grid[0])-1; c++ {
+			if grid[r][c] == '@' {
+				adj := numAdjacent(r, c, grid)
+				if adj < 4 {
+					total++
+				}
+			}
+		}
+	}
+	fmt.Println(total)
+}
+
+func part2(grid [][]rune) {
+	total := 0
+
+	for {
+		subtotal := 0
+		coords := make([][2]int, 0)
+
+		for r := 1; r < len(grid)-1; r++ {
+			for c := 1; c < len(grid[0])-1; c++ {
+				if grid[r][c] == '@' {
+					adj := numAdjacent(r, c, grid)
+					if adj < 4 {
+						subtotal++
+						coords = append(coords, [2]int{r, c})
+					}
+				}
+			}
+		}
+		if subtotal == 0 {
+			break
+		}
+		total += subtotal
+
+		// Update the grid
+		for _, coord := range coords {
+			nr, nc := coord[0], coord[1]
+			grid[nr][nc] = '.'
+		}
+	}
+
+	fmt.Println(total)
+}
+
+func numAdjacent(r, c int, grid [][]rune) int {
+	count := 0
+
+	coords := [][2]int{
+		{r - 1, c - 1},
+		{r - 1, c},
+		{r - 1, c + 1},
+		{r, c - 1},
+		{r, c + 1},
+		{r + 1, c - 1},
+		{r + 1, c},
+		{r + 1, c + 1},
+	}
+
+	for _, coord := range coords {
+		if grid[coord[0]][coord[1]] == '@' {
+			count++
+		}
+	}
+
+	return count
+}
+
+func parseInput(filename string) [][]rune {
+	data, err := os.ReadFile(filename)
+	if err != nil || len(data) == 0 {
+		log.Fatalln("unable to read file:", filename)
+	}
+
+	var grid [][]rune
+	lines := strings.Split(string(data), "\n")
+	n := len(lines[0])
+
+	// Add a buffer around all 4 sides for ease of checking adjacency
+	grid = append(grid, newBufferLine(n+2, '.'))
+
+	for _, line := range lines {
+		extLine := "." + line + "."
+		grid = append(grid, []rune(extLine))
+	}
+
+	grid = append(grid, newBufferLine(n+2, '.'))
+	return grid
+}
+
+func newBufferLine(size int, char rune) []rune {
+	bufferLine := make([]rune, size)
+	for i := 0; i < len(bufferLine); i++ {
+		bufferLine[i] = char
+	}
+
+	return bufferLine
+}
