87ceab7b38e086ab56d3e5a8f70d794c41f998f3
Dennis Ping <idennisping@gmail.com>
2025-12-08 21:21:55 -0500

    Completed day 6

--- Day6/main.go
+++ Day6/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -13,25 +14,36 @@ type MathProblem struct {
 	op     rune
 }
 
-func main() {
-	mps := parseInput("input.txt")
-	part1(mps)
+type OpPosition struct {
+	op    rune
+	index int
 }
 
-func part1(mps []MathProblem) {
+func main() {
+	mathProblems := parseInput1("input.txt")
+	calculate(mathProblems)
+
+	mathProblems = parseInput2("input.txt")
+	calculate(mathProblems)
+}
+
+func calculate(mps []MathProblem) {
 	total := 0
 	for _, mp := range mps {
 		var subtotal int
-		if mp.op == '+' {
+		switch mp.op {
+		case '+':
 			subtotal = 0
 			for _, value := range mp.values {
 				subtotal += value
 			}
-		} else if mp.op == '*' {
+		case '*':
 			subtotal = 1
 			for _, value := range mp.values {
 				subtotal *= value
 			}
+		default:
+			log.Fatalln("unknown operator:", mp.op)
 		}
 		total += subtotal
 	}
@@ -39,7 +51,7 @@ func part1(mps []MathProblem) {
 	fmt.Println(total)
 }
 
-func parseInput(filename string) []MathProblem {
+func parseInput1(filename string) []MathProblem {
 	data, err := os.ReadFile(filename)
 	if err != nil || len(data) == 0 {
 		log.Fatalln("unable to read file:", filename)
@@ -67,6 +79,74 @@ func parseInput(filename string) []MathProblem {
 	opsLine := lines[len(lines)-1]
 	for k, op := range strings.Fields(opsLine) {
 		mathProblems[k].op = rune(op[0]) // The operator is always 1 char long
+	}
+
+	return mathProblems
+}
+
+func parseInput2(filename string) []MathProblem {
+	data, err := os.ReadFile(filename)
+	if err != nil || len(data) == 0 {
+		log.Fatalln("unable to read file:", filename)
+	}
+
+	lines := strings.Split(string(data), "\n")
+
+	// Extract the operator line to figure out the start and end of each column
+	opLine := lines[len(lines)-1]
+	opPos := make([]OpPosition, 0)
+	for i, char := range opLine {
+		if char != ' ' {
+			switch char {
+			case '*', '+':
+				opPos = append(opPos, OpPosition{char, i})
+			default:
+				log.Fatalln("unknown operator:", char)
+			}
+		}
+	}
+
+	// Append a terminator (.) to the end of opLine so we can loop easily
+	opPos = append(opPos, OpPosition{'.', len(opLine)})
+
+	// Find the longest line
+	maxLength := 0
+	for _, line := range lines[:len(lines)-1] {
+		maxLength = max(maxLength, len(line))
+	}
+
+	// Pad right with spaces to align the last column
+	for i := 0; i < len(lines)-1; i++ {
+		diff := maxLength - len(lines[i])
+		padding := string(slices.Repeat([]rune{' '}, diff))
+		lines[i] += padding
+	}
+
+	numProblems := len(opPos) - 1
+	mathProblems := make([]MathProblem, numProblems)
+
+	for i := 0; i < len(opPos); i++ {
+		if opPos[i].op == '.' {
+			break // terminate
+		}
+		start := opPos[i].index
+		end := opPos[i+1].index
+
+		// Look down each column and build the number
+		for c := start; c < end; c++ {
+			word := ""
+			for r := 0; r < len(lines)-1; r++ {
+				if lines[r][c] != ' ' {
+					word += string(lines[r][c])
+				}
+			}
+			// Convert the word to integer and append to list of values
+			if len(word) > 0 {
+				value, _ := strconv.Atoi(word)
+				mathProblems[i].values = append(mathProblems[i].values, value)
+			}
+		}
+		mathProblems[i].op = opPos[i].op
 	}
 
 	return mathProblems
