85f0b1fae86c121307c9f7a4cb03f8614b6ed91f
Dennis Ping <idennisping@gmail.com>
2025-12-10 12:11:35 -0500

    Completed day 7

--- Day7/main.go
+++ Day7/main.go
@@ -13,6 +13,7 @@ import (
 func main() {
 	grid := parseInput("input.txt")
 	part1(grid)
+	part2(grid)
 }
 
 func part1(grid []string) {
@@ -22,39 +23,82 @@ func part1(grid []string) {
 	beamIdx := make([]int, 0) // Beam indices
 	beamIdx = append(beamIdx, start)
 
-	for r := 2; r < len(grid)-1; r++ {
-		for c := 0; c < len(grid[0]); c++ {
-			// If odd row, then skip
-			if r%2 != 0 {
-				continue
-			}
+	// Only even rows matter, skip odd rows
+	for r := 2; r < len(grid)-1; r += 2 {
+		splitterIdx := utils.FindAllIndices([]rune(grid[r]), '^')
 
-			splitterIdx := utils.FindAllIndices([]rune(grid[r]), '^')
-			for _, splitter := range splitterIdx {
-				for j, beam := range beamIdx {
-					if splitter == beam {
-						// Delete the old beam
-						beamIdx = slices.Delete(beamIdx, j, j+1)
+		for _, splitter := range splitterIdx {
+			for j, beam := range beamIdx {
+				if splitter == beam {
+					// Delete the old beam
+					beamIdx = slices.Delete(beamIdx, j, j+1)
 
-						// Add the left beam
-						if !slices.Contains(beamIdx, beam-1) {
-							beamIdx = append(beamIdx, beam-1)
-						}
-
-						// Add the right beam
-						if !slices.Contains(beamIdx, beam+1) {
-							beamIdx = append(beamIdx, beam+1)
-						}
-
-						count++
+					// Add the left beam
+					if beam-1 >= 0 && !slices.Contains(beamIdx, beam-1) {
+						beamIdx = append(beamIdx, beam-1)
 					}
+
+					// Add the right beam
+					if beam+1 < len(grid[0]) && !slices.Contains(beamIdx, beam+1) {
+						beamIdx = append(beamIdx, beam+1)
+					}
+
+					count++
 				}
 			}
-
 		}
 	}
 
 	fmt.Println(count)
+}
+
+func part2(grid []string) {
+	// Don't build a polytree and do DFS. Too complex.
+	// Instead, track how many beams currently exist in each column as we move downwards.
+	// Each row is a time step. The # of beams in each column == # of possibilities.
+
+	length := len(grid)
+	width := len(grid[0])
+	start := strings.Index(grid[0], "S")
+
+	beams := make([]int, width) // Array of beam counts in each position
+	beams[start] = 1
+
+	for r := 2; r < length; r += 2 {
+		row := grid[r]
+		next := make([]int, width) // Array of next beam counts in each position
+
+		for c := 0; c < width; c++ {
+			k := beams[c]
+			if k == 0 {
+				continue // No beams to propagate down
+			}
+
+			if c < len(row) && row[c] == '^' {
+				// Add count k to left column
+				if c-1 >= 0 {
+					next[c-1] += k
+				}
+
+				// Add count k to right column
+				if c+1 < width {
+					next[c+1] += k
+				}
+			} else {
+				// Propagate straight down
+				next[c] += k
+			}
+		}
+
+		beams = next
+	}
+
+	totalPaths := 0
+	for _, k := range beams {
+		totalPaths += k
+	}
+
+	fmt.Println(totalPaths)
 }
 
 func parseInput(filename string) []string {
